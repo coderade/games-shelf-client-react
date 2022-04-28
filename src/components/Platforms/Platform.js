46ab0e2003bbf72a63e2eb62fcd63669768056dd
Valdeci Gomes <contact@valde.ci>
2022-04-28 19:34:52 -0300

    Change platforms components to functional components

--- src/components/Platforms/Platform.js
+++ src/components/Platforms/Platform.js
@@ -1,57 +1,38 @@
-import React, {Component, Fragment} from "react";
-import ShelfService from "../../services/ShelfService";
+import React, {Fragment, useEffect, useState} from "react";
 import {Link, useLocation, useParams} from "react-router-dom"
+import ShelfService from "../../services/ShelfService";
 
-class Platform extends Component {
-    state = {
-        games: [], platformName: '', isLoaded: false, error: null
-    }
+const Platform = () => {
+    const location = useLocation();
+    const {id} = useParams()
+    const [games, setGames] = useState([])
+    const [error, setError] = useState("")
 
-    componentDidMount() {
-        const platformId = this.props.params.id;
-        ShelfService.getAllGames(null, platformId)
+    useEffect((id) => {
+        ShelfService.getAllGames(null, id)
             .then(result => {
-                this.setState({
-                    games: result.games, isLoaded: true, platformName: this.props.location.state.platformName
-                })
+                setGames(result.games)
             })
             .catch(err => {
                 const errorMessage = `Error loading games by platform: ${err}`;
-                this.setState({error: errorMessage, isLoaded: true})
+                setError(errorMessage)
             })
-    }
+    }, [id])
 
-    render() {
-        let {games, isLoaded, platformName, error} = this.state;
-        if(!games) games =[];
+    if (error) {
+        return <div className="error-message">{error}</div>
+    } else {
+        return (<Fragment>
+            <h2>Platform: {location.state.platformName} </h2>
+            <div className="list-group">
+                {games.map(game => (
+                    <Link to={`/games/${game.id}`} className="list-group-item list-group-item-action" key={game.id}>
+                        {game.title}
+                    </Link>))}
+            </div>
 
-        if (error) {
-            return <div className="error-message">{error}</div>
-        } else if (!isLoaded) {
-            return <p>Loading...</p>
-        } else {
-            return (<Fragment>
-                <h2>Platform: {platformName} </h2>
-                <div className="list-group">
-                    {games.map(game => (
-                        <Link to={`/games/${game.id}`} className="list-group-item list-group-item-action" key={game.id}>
-                            {game.title}
-                        </Link>
-                    ))}
-                </div>
-
-            </Fragment>);
-        }
+        </Fragment>);
     }
 }
 
-function withRouter(Component) {
-    function ComponentWithRouter(props) {
-        let params = useParams()
-        const location = useLocation();
-        return <Component {...props} params={params} location={location} />
-    }
-    return ComponentWithRouter
-}
-
-export default withRouter(Platform);
+export default Platform;
--- src/components/Platforms/Platforms.js
+++ src/components/Platforms/Platforms.js
@@ -1,43 +1,36 @@
-import React, {Component, Fragment} from 'react'
+import React, {Component, Fragment, useEffect, useState} from 'react'
 import {Link} from "react-router-dom";
 import ShelfService from "../../services/ShelfService";
 
-export default class Platforms extends Component {
-    state = {
-        platforms: [], isLoaded: false, error: null
-    }
+const Platforms = () => {
+    const [platforms, setPlatforms] = useState([])
+    const [error, setError] = useState("")
 
-    componentDidMount() {
+    useEffect(() => {
         ShelfService.getPlatforms()
             .then(result => {
-                this.setState({
-                    platforms: result.platforms, isLoaded: true
-                })
+                setPlatforms(result.platforms)
             })
             .catch(err => {
                 const errorMessage = `Error loading platforms: ${err.status} - ${err.statusText}`;
-                this.setState({error: errorMessage, isLoaded: true})
+                setError(errorMessage)
             })
-    }
+    }, [])
 
-    render() {
-        const {platforms, isLoaded, error} = this.state;
-
-        if (error) {
-            return <div className="error-message">{error}</div>
-        } else if (!isLoaded) {
-            return <p>Loading...</p>
-        } else {
-            return (<Fragment>
-                <h2>Platforms</h2>
-                <div>
-                    {platforms.map(platform => (
-                        <Link key={platform.id} to={`/platforms/${platform.id}`} state={{platformName: platform.name}}
-                              className="list-group-item list-group-item-action">
-                            {platform.name}
-                        </Link>))}
-                </div>
-            </Fragment>)
-        }
+    if (error) {
+        return <div className="error-message">{error}</div>
+    } else {
+        return (<Fragment>
+            <h2>Platforms</h2>
+            <div>
+                {platforms.map(platform => (
+                    <Link key={platform.id} to={`/platforms/${platform.id}`} state={{platformName: platform.name}}
+                          className="list-group-item list-group-item-action">
+                        {platform.name}
+                    </Link>))}
+            </div>
+        </Fragment>)
     }
 }
+
+export default Platforms;
